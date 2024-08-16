aa5605b8240faa52a7579ab1353f75f863a24439
AqibAamir <144609363+AqibAamir@users.noreply.github.com>
2024-08-16 18:11:55 +0100

    Update students.py

--- students.py
+++ students.py
@@ -180,3 +180,53 @@ class App(customtkinter.CTk):
         new_scaling = "120%"
         new_scaling_float = int(new_scaling.replace("%", "")) / 100
         customtkinter.set_widget_scaling(new_scaling_float)
+
+
+        # create tabview
+        self.tabview = customtkinter.CTkTabview(self, width=900, height=600)
+        self.tabview.grid(row=0, column=1, padx=(20, 20), pady=(20, 20), sticky="nsew")
+        self.tabview.add("Behaviour Report")
+        self.tabview.add("Spend XP")
+        self.tabview.add("Award XP")
+        self.tabview.tab("Behaviour Report").grid_columnconfigure(0, weight=1)  # configure grid of individual tabs
+        self.tabview.tab("Spend XP").grid_columnconfigure(0, weight=1)
+        self.tabview.tab("Award XP").grid_columnconfigure(0, weight=1)
+
+        self.names = customtkinter.CTkLabel(self.tabview.tab("Behaviour Report"), text="Welcome to the Behaviour System!", width=500, height=40, font=customtkinter.CTkFont(size=35, weight="bold"))
+        self.names.grid(row=1, column=0, padx=20, pady=(20, 10))
+        self.names1 = customtkinter.CTkLabel(self.tabview.tab("Behaviour Report"), text="Click on a year group on the side to get started.", font=customtkinter.CTkFont(size=20), width=500, height=40)
+        self.names1.grid(row=2, column=0, padx=20, pady=(20, 10))
+
+        self.names11 = customtkinter.CTkLabel(self.tabview.tab("Spend XP"), text="Welcome to the XP Spending Tab!", width=500, height=40, font=customtkinter.CTkFont(size=35, weight="bold"))
+        self.names11.grid(row=1, column=0, padx=20, pady=(20, 10))
+        self.names111 = customtkinter.CTkLabel(self.tabview.tab("Spend XP"), text="Click on a year group on the side to get started.", font=customtkinter.CTkFont(size=20), width=500, height=40)
+        self.names111.grid(row=2, column=0, padx=20, pady=(20, 10))
+
+        self.names112 = customtkinter.CTkLabel(self.tabview.tab("Award XP"), text="Welcome to the XP Awarding Tab!", width=500, height=40, font=customtkinter.CTkFont(size=35, weight="bold"))
+        self.names112.grid(row=1, column=0, padx=20, pady=(20, 10))
+        self.names1112 = customtkinter.CTkLabel(self.tabview.tab("Award XP"), text="Click on a year group on the side to get started.", font=customtkinter.CTkFont(size=20), width=500, height=40)
+        self.names1112.grid(row=2, column=0, padx=20, pady=(20, 10))
+
+        # set default values
+    
+    def updatlol(self):
+        pass
+
+    def year13sidebutton(self):
+        print("Year 13 button pressed")
+
+    def year12sidebutton(self):
+        print("Year 12 button pressed")
+
+    def year11sidebutton(self):
+        print("Year 11 button pressed")
+
+    def year10sidebutton(self):
+        print("Year 10 button pressed")
+
+    def year9sidebutton(self):
+        print("Year 9 button pressed")
+
+if __name__ == "__main__":
+    app = App()
+    app.mainloop()
